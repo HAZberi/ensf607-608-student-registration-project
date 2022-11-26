f004f76d2ea13fdc748e81964554a8dfd89fa25c
Hassaan Zuberi <mhazuberi@gmail.com>
2022-11-26 15:55:29 -0700

    commented out course for time being

--- src/main/java/com/student_reg/demo/course/Course.java
+++ src/main/java/com/student_reg/demo/course/Course.java
@@ -1,129 +1,129 @@
-package com.student_reg.demo.course;
+// package com.student_reg.demo.course;
 
-import com.student_reg.demo.student.*;
+// import com.student_reg.demo.student.*;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+// import javax.persistence.*;
+// import java.io.Serializable;
+// import java.time.LocalDate;
+// import java.util.HashSet;
+// import java.util.Set;
 
-@Entity
-@Table(name = "course")
-public class Course implements Serializable {
+// @Entity
+// @Table(name = "course")
+// public class Course implements Serializable {
 
-    @Id
-    @SequenceGenerator(
-            name = "course_sequence",
-            sequenceName = "course_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "course_sequence"
-    )
-    private int id;
+//     @Id
+//     @SequenceGenerator(
+//             name = "course_sequence",
+//             sequenceName = "course_sequence",
+//             allocationSize = 1
+//     )
+//     @GeneratedValue(
+//             strategy = GenerationType.SEQUENCE,
+//             generator = "course_sequence"
+//     )
+//     private int id;
 
-    private String name;
+//     private String name;
 
-    private Integer capacity;
+//     private Integer capacity;
 
-    private Boolean hasPrerequisite;
+//     private Boolean hasPrerequisite;
 
-    @ManyToMany
-    @JoinTable(
-            name = "student_enrolled",
-            joinColumns = @JoinColumn(name = "course_id"),
-            inverseJoinColumns = @JoinColumn(name = "student_id")
-    )
-    private Set<Student> enrolledStudents = new HashSet<>();
+//     @ManyToMany
+//     @JoinTable(
+//             name = "student_enrolled",
+//             joinColumns = @JoinColumn(name = "course_id"),
+//             inverseJoinColumns = @JoinColumn(name = "student_id")
+//     )
+//     private Set<Student> enrolledStudents = new HashSet<>();
 
-    public Course() {
-    }
+//     public Course() {
+//     }
 
-    public Course(Long id, String name, LocalDate startTime, LocalDate endTime, Integer capacity, Boolean hasPrerequisite, Set<Student> enrolledStudents) {
-        this.id = id;
-        this.name = name;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.capacity = capacity;
-        this.hasPrerequisite = hasPrerequisite;
-        this.enrolledStudents = enrolledStudents;
-    }
+//     public Course(Long id, String name, LocalDate startTime, LocalDate endTime, Integer capacity, Boolean hasPrerequisite, Set<Student> enrolledStudents) {
+//         this.id = id;
+//         this.name = name;
+//         this.startTime = startTime;
+//         this.endTime = endTime;
+//         this.capacity = capacity;
+//         this.hasPrerequisite = hasPrerequisite;
+//         this.enrolledStudents = enrolledStudents;
+//     }
 
-    public Course(String name, LocalDate startTime, LocalDate endTime, Integer capacity, Boolean hasPrerequisite, Set<Student> enrolledStudents) {
-        this.name = name;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.capacity = capacity;
-        this.hasPrerequisite = hasPrerequisite;
-        this.enrolledStudents = enrolledStudents;
-    }
+//     public Course(String name, LocalDate startTime, LocalDate endTime, Integer capacity, Boolean hasPrerequisite, Set<Student> enrolledStudents) {
+//         this.name = name;
+//         this.startTime = startTime;
+//         this.endTime = endTime;
+//         this.capacity = capacity;
+//         this.hasPrerequisite = hasPrerequisite;
+//         this.enrolledStudents = enrolledStudents;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public Course setId(Long id) {
-        this.id = id;
-        return this;
-    }
+//     public Course setId(Long id) {
+//         this.id = id;
+//         return this;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public Course setName(String name) {
-        this.name = name;
-        return this;
-    }
+//     public Course setName(String name) {
+//         this.name = name;
+//         return this;
+//     }
 
-    public LocalDate getStartTime() {
-        return startTime;
-    }
+//     public LocalDate getStartTime() {
+//         return startTime;
+//     }
 
-    public Course setStartTime(LocalDate startTime) {
-        this.startTime = startTime;
-        return this;
-    }
+//     public Course setStartTime(LocalDate startTime) {
+//         this.startTime = startTime;
+//         return this;
+//     }
 
-    public LocalDate getEndTime() {
-        return endTime;
-    }
+//     public LocalDate getEndTime() {
+//         return endTime;
+//     }
 
-    public Course setEndTime(LocalDate endTime) {
-        this.endTime = endTime;
-        return this;
-    }
+//     public Course setEndTime(LocalDate endTime) {
+//         this.endTime = endTime;
+//         return this;
+//     }
 
-    public Integer getCapacity() {
-        return capacity;
-    }
+//     public Integer getCapacity() {
+//         return capacity;
+//     }
 
-    public Course setCapacity(Integer capacity) {
-        this.capacity = capacity;
-        return this;
-    }
+//     public Course setCapacity(Integer capacity) {
+//         this.capacity = capacity;
+//         return this;
+//     }
 
-    public Boolean getHasPrerequisite() {
-        return hasPrerequisite;
-    }
+//     public Boolean getHasPrerequisite() {
+//         return hasPrerequisite;
+//     }
 
-    public Course setHasPrerequisite(Boolean hasPrerequisite) {
-        this.hasPrerequisite = hasPrerequisite;
-        return this;
-    }
+//     public Course setHasPrerequisite(Boolean hasPrerequisite) {
+//         this.hasPrerequisite = hasPrerequisite;
+//         return this;
+//     }
 
-    public Set<Student> getEnrolledStudents() {
-        return enrolledStudents;
-    }
+//     public Set<Student> getEnrolledStudents() {
+//         return enrolledStudents;
+//     }
 
-    public Course setEnrolledStudents(Set<Student> enrolledStudents) {
-        this.enrolledStudents = enrolledStudents;
-        return this;
-    }
+//     public Course setEnrolledStudents(Set<Student> enrolledStudents) {
+//         this.enrolledStudents = enrolledStudents;
+//         return this;
+//     }
 
-    public void enrolledStudents(Student student) {
-        enrolledStudents.add(student);
-    }
-}
+//     public void enrolledStudents(Student student) {
+//         enrolledStudents.add(student);
+//     }
+// }
--- src/main/java/com/student_reg/demo/course/CourseService.java
+++ src/main/java/com/student_reg/demo/course/CourseService.java
@@ -1,42 +1,42 @@
-package com.student_reg.demo.course;
+// package com.student_reg.demo.course;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-@Service
-public class CourseService {
+// @Service
+// public class CourseService {
 
-    private final CourseRepository courseRepository;
+//     private final CourseRepository courseRepository;
 
-    @Autowired
-    public CourseService(CourseRepository courseRepository) {
-        this.courseRepository = courseRepository;
-    }
+//     @Autowired
+//     public CourseService(CourseRepository courseRepository) {
+//         this.courseRepository = courseRepository;
+//     }
 
-    public List<Course> getAllCourses() {
-        return courseRepository.findAll();
-    }
+//     public List<Course> getAllCourses() {
+//         return courseRepository.findAll();
+//     }
 
-    public void addNewCourse(Course course) {
-        Optional<Course> courseByName = courseRepository.findByName(course.getName());
-        if (courseByName.isPresent()) {
-            throw new IllegalStateException("Course already exist!");
-        }
-        courseRepository.save(course);
-    }
+//     public void addNewCourse(Course course) {
+//         Optional<Course> courseByName = courseRepository.findByName(course.getName());
+//         if (courseByName.isPresent()) {
+//             throw new IllegalStateException("Course already exist!");
+//         }
+//         courseRepository.save(course);
+//     }
 
-    public void updateCourse(Course course) {
-        courseRepository.save(course);
-    }
+//     public void updateCourse(Course course) {
+//         courseRepository.save(course);
+//     }
 
-    public Course getCourseById(Integer courseId) {
-        Optional<Course> courseById = courseRepository.findById(courseId);
-        if (!courseById.isPresent()) {
-            throw new IllegalStateException("Course does'nt exist!");
-        }
-        return courseById.get();
-    }
-}
+//     public Course getCourseById(Integer courseId) {
+//         Optional<Course> courseById = courseRepository.findById(courseId);
+//         if (!courseById.isPresent()) {
+//             throw new IllegalStateException("Course does'nt exist!");
+//         }
+//         return courseById.get();
+//     }
+// }
