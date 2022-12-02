e7a01765c091a40bd98802b4f1e098a13bd4666e
Hassaan Zuberi <mhazuberi@gmail.com>
2022-12-02 00:58:16 -0700

    controller setup init

--- frontend/src/NotSignedIn.js
+++ frontend/src/NotSignedIn.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Form, Row, Col, Label, Input, Button } from "reactstrap";
-import { Link } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth";
 import { getStudent } from "./controllers/appController";
 
