1f97f2693167a7cf24da67235b75f3ba2053ca51
Hassaan Zuberi <mhazuberi@gmail.com>
2022-12-02 03:02:31 -0700

    student login session implemented

--- frontend/src/NotSignedIn.js
+++ frontend/src/NotSignedIn.js
@@ -2,41 +2,57 @@ import React from "react";
 import { Form, Row, Col, Label, Input, Button } from "reactstrap";
 import { useAuth } from "./hooks/useAuth";
 import { getStudent } from "./controllers/appController";
+import { ToastContainer, toast } from "react-toastify";
 
 function NotSignedIn(args) {
-
   const { login } = useAuth();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const userId = e.target.studentId.value;
-    getStudent(userId).then(student => {
-      login(student);
+    getStudent(userId).then((student) => {
       e.target.reset();
+      if (!student.error) {
+        login(student);
+      } else {
+        toast.error(student.message, {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          theme: "colored",
+        });
+      }
     });
   };
 
   return (
-    <div className="d-flex justify-content-center mt-5">
-      <Form onSubmit={handleSubmit}>
-        <Row className="row-cols-lg-auto g-3 align-items-center">
-          <Col>
-            <Label className="visually-hidden" for="exampleEmail">
-              Student ID
-            </Label>
-            <Input
-              id="studentId"
-              name="studentId"
-              placeholder="Enter StudentID"
-              type="text"
-            />
-          </Col>
-          <Col>
-            <Button color="btn btn-primary" type="submit">Submit</Button>
-          </Col>
-        </Row>
-      </Form>
-    </div>
+    <>
+      <div className="d-flex justify-content-center mt-5">
+        <Form onSubmit={handleSubmit}>
+          <Row className="row-cols-lg-auto g-3 align-items-center">
+            <Col>
+              <Label className="visually-hidden" for="exampleEmail">
+                Student ID
+              </Label>
+              <Input
+                id="studentId"
+                name="studentId"
+                placeholder="Enter StudentID"
+                type="text"
+              />
+            </Col>
+            <Col>
+              <Button color="btn btn-primary" type="submit">
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+      <ToastContainer />
+    </>
   );
 }
 
